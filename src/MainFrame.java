c5387f138b56be1a19e4d6d61e9ec63eb49b4aeb
caichao <caichao08@gmail.com>
2018-08-16 21:11:10 +0800

    8/16

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,7 +1,9 @@
 import org.jb2011.lnf.beautyeye.ch3_button.BEButtonUI;
+import org.json.JSONObject;
 
 import javax.sound.sampled.Line;
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicArrowButton;
 import java.awt.*;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -13,7 +15,7 @@ public class MainFrame extends JFrame implements Runnable,ActionListener,Observe
     private DrawMainFrame drawMainFrame = null;
     private ParticleFilter particleFilter = null;
     private boolean isThreaAlive = true;
-    float scale = 185;
+    float scale = 185;              //放大尺度
     int originX = 35;               //原点的x
     int originY = 520;              //原点的y
     int xAxisLength = 900;         //X轴长度
@@ -44,6 +46,195 @@ public class MainFrame extends JFrame implements Runnable,ActionListener,Observe
         //imgLabel.setBounds(290, 20, screenWidth, screenHeight);//图片标签(地图区域)的位置和大小
 
 
+        Container contain = this.getContentPane();
+        /*
+        * 画出参数编辑区域
+        * */
+        JPanel paramConfig = new JPanel();
+        paramConfig.setBackground(Color.gray);
+        paramConfig.setLayout(new GridLayout());
+        paramConfig.setBounds(50,560,700,252);
+        this.getLayeredPane().add(paramConfig);
+        paramConfig.setLayout(null);
+
+        JButton j1 = new JButton("Location");
+        paramConfig.add(j1);
+        //j1.setSize(50,30);
+        j1.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.lightBlue));
+        j1.setBounds(new Rectangle(1, 1, 100, 50));
+
+
+        JTextArea x = new JTextArea("    X");
+        x.setEditable(false);
+        paramConfig.add(x);
+        x.setBounds(new Rectangle(101, 1, 100, 50));
+
+
+        JTextArea y = new JTextArea("    Y");
+        y.setEditable(false);
+        paramConfig.add(y);
+        y.setBounds(new Rectangle(201, 1, 100, 50));
+
+        JTextArea z = new JTextArea("    Z");
+        z.setEditable(false);
+        paramConfig.add(z);
+        z.setBounds(new Rectangle(301, 1, 100, 50));
+
+        JTextArea anchorItem0 = new JTextArea("anchor 0");
+        anchorItem0.setEditable(false);
+        paramConfig.add(anchorItem0);
+        anchorItem0.setBounds(new Rectangle(1, 51, 100, 50));
+
+        JTextField textx0 = new JTextField();//x0
+        paramConfig.add(textx0);
+        textx0.setBounds(101,51,100,50);
+        textx0.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextField texty0 = new JTextField();//y0
+        paramConfig.add(texty0);
+        texty0.setBounds(201,51,100,50);
+        texty0.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextField textz0 = new JTextField();//z0
+        paramConfig.add(textz0);
+        textz0.setBounds(301,51,100,50);
+        textz0.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextArea anchorItem1 = new JTextArea("anchor 1");
+        anchorItem1.setEditable(false);
+        paramConfig.add(anchorItem1);
+        anchorItem1.setBounds(1,101,100,50);
+
+        JTextField textx1 = new JTextField();//x1
+        paramConfig.add(textx1);
+        textx1.setBounds(101,101,100,50);
+        textx1.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextField texty1 = new JTextField();//y1
+        paramConfig.add(texty1);
+        texty1.setBounds(201,101,100,50);
+        texty1.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextField textz1 = new JTextField();//z1
+        paramConfig.add(textz1);
+        textz1.setBounds(301,101,100,50);
+        textz1.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextArea anchorItem2 = new JTextArea("anchor 2");
+        anchorItem2.setEditable(false);
+        paramConfig.add(anchorItem2);
+        anchorItem2.setBounds(1,151,100,50);
+
+        JTextField textx2 = new JTextField();//x2
+        paramConfig.add(textx2);
+        textx2.setBounds(101,151,100,50);
+        textx2.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextField texty2 = new JTextField();//y2
+        paramConfig.add(texty2);
+        texty2.setBounds(201,151,100,50);
+        texty2.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextField textz2= new JTextField();//z2
+        paramConfig.add(textz2);
+        textz2.setBounds(301,151,100,50);
+        textz2.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextArea anchorItem3 = new JTextArea("anchor 3");
+        anchorItem3.setEditable(false);
+        paramConfig.add(anchorItem3);
+        anchorItem3.setBounds(1,201,100,50);
+
+
+        JTextField textx3 = new JTextField();//x3
+        paramConfig.add(textx3);
+        textx3.setBounds(101,201,100,50);
+        textx3.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextField texty3 = new JTextField();//y3
+        paramConfig.add(texty3);
+        texty3.setBounds(201,201,100,50);
+        texty3.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextField textz3= new JTextField();//z3
+        paramConfig.add(textz3);
+        textz3.setBounds(301,201,100,50);
+        textz3.setHorizontalAlignment(JTextField.CENTER);
+
+        JButton title2 = new JButton("    Other Parameters");
+        paramConfig.add(title2);
+        title2.setBounds(new Rectangle(402, 1, 297, 50));
+
+        JTextField textScale = new JTextField("Scale");
+        textScale.setEditable(false);
+        paramConfig.add(textScale);
+        textScale.setBounds(new Rectangle(402, 51, 140, 50));
+        textScale.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextField textTargetHeight = new JTextField("Target Height");
+        textTargetHeight.setEditable(false);
+        paramConfig.add(textTargetHeight);
+        textTargetHeight.setBounds(new Rectangle(402, 101, 140, 50));
+        textTargetHeight.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextField textInterval = new JTextField("Schedule Interval");
+        textInterval.setEditable(false);
+        paramConfig.add(textInterval);
+        textInterval.setBounds(new Rectangle(402, 151, 140, 50));
+        textInterval.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextField scaleText = new JTextField();  //scale
+        paramConfig.add(scaleText);
+        scaleText.setBounds(542,51,157,50);
+        scaleText.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextField heightText = new JTextField();  //height
+        paramConfig.add(heightText);
+        heightText.setBounds(542,101,157,50);
+        heightText.setHorizontalAlignment(JTextField.CENTER);
+
+        JTextField intervalText = new JTextField();  //interval
+        paramConfig.add(intervalText);
+        intervalText.setBounds(542,151,157,50);
+        intervalText.setHorizontalAlignment(JTextField.CENTER);
+
+
+
+        JButton config = new JButton("Config");
+        paramConfig.add(config);
+        config.setBounds(542,201,157,50);
+
+        JButton load = new JButton("Load");
+        paramConfig.add(load);
+
+        JSONObject jsonObject = new JSONObject();
+        JSONObject temp = null;
+        //temp = jsonObject.getJSONObject("anchor" );
+
+//        JTextField blank = new JTextField();//x0
+//        paramConfig.add(blank);
+//        blank.setSize(100,50);
+//        blank.setHorizontalAlignment(JTextField.CENTER);
+
+
+
+
+
+
+
+
+
+
+
+//        JButton j = new JButton("123");
+//        //j.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.lightBlue));
+//        this.getLayeredPane().add(j);
+//        j.setBounds(1000,800,30,30);
+
+
+
+
+
 
         /*
          * 设置按钮
@@ -63,15 +254,9 @@ public class MainFrame extends JFrame implements Runnable,ActionListener,Observe
         KillAll.addActionListener(L2);
 
 
-        //new BulbControl();
 
 
-        //this.getLayeredPane().add(new MyPanel2(), new Integer(Integer.MIN_VALUE));
-
-        // 设置背景标签的位置
-        Container contain = this.getContentPane();
-
-        ((JPanel) contain).setOpaque(false);
+        //((JPanel) contain).setOpaque(false);
         // 将内容面板设为透明。将LayeredPane面板中的背景显示出来。
 
         //this.add(new MyPanel2());
@@ -303,6 +488,7 @@ public class MainFrame extends JFrame implements Runnable,ActionListener,Observe
             g2.setColor(Color.BLACK);
             g2.setStroke(new BasicStroke(1.5f));
             drawAnchor(AnchorScale, g2);
+            initAnchorState(g2);
             if(isMessageRecv) {
                 //标记当前发出消息的anchor
                 freshAnchorState(g2, currentActivateAnchorId);
@@ -386,6 +572,9 @@ public class MainFrame extends JFrame implements Runnable,ActionListener,Observe
 
         }
 
+
+
+        //画当前发送消息的anchor的指示灯
         public void freshAnchorState(Graphics2D g,int AnchorNum) {
             g.setFont(new Font("MS 明朝", Font.BOLD, 20));
             g.setStroke(new BasicStroke(4f));
@@ -424,22 +613,36 @@ public class MainFrame extends JFrame implements Runnable,ActionListener,Observe
 
         }
 
+
+        public void initAnchorState(Graphics2D g){
+            g.setFont(new Font("宋体", Font.BOLD, 20));
+            g.setColor(Color.BLACK);
+            g.drawString("Anchor state",1150,140);
+            g.drawString("Anchor_0:",1000,180);
+            g.drawString("Anchor_1:",1000,230);
+            g.drawString("Anchor_2:",1000,280);
+            g.drawString("Anchor_3:",1000,330);
+            g.setFont(new Font("宋体", Font.BOLD, 40));
+            g.setColor(Color.red);
+            for(int i =0;i<4;i++){
+                g.drawString("■",1200,185+i*50);
+            }
+
+        }
+
+
         public void drawAnchorWorkingState(Graphics2D g)
         {
-            g.setFont(new Font("宋体", Font.BOLD, 40));
-            g.setColor(Color.BLACK);
-            g.drawString("Anchor_0:",1000,150);
-            g.drawString("Anchor_1:",1000,200);
-            g.drawString("Anchor_2:",1000,250);
-            g.drawString("Anchor_3:",1000,300);
+
             for(int i =0;i<4;i++){
+                g.setFont(new Font("宋体", Font.BOLD, 40));
                 if(isAnchorWorking[i]==true){
                     g.setColor(Color.green);
-                    g.drawString("●",1100,150+i*50);
+                    g.drawString("●",1200,185+i*50);
                 }
                 else{
                     g.setColor(Color.red);
-                    g.drawString("■",1100,150+i*50);
+                    g.drawString("■",1200,185+i*50);
                 }
             }
         }
